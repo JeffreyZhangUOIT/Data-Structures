81e54395bfca2eb4d82dcb04518102a1e3f6fe41
Jeffrey Zhang <jeffrey.zhang@uoit.net>
2017-04-04 23:59:04 -0400

    JEFF-DATED

--- k mean.py
+++ k mean.py
@@ -28,42 +28,38 @@ with open('exercise-1.csv') as csvfile:
         pair.append((z, y))
 
 
-    # Print the coordinate list
+        # Print the coordinate list
 print (pair)  # Ex.: pair[0][0] for the x  data in the 0 index
 print(len(pair))
 
-
-import numpy as np
-import random
-
-pair2= random.sample(pair,150)
-K=4
-pair= data
-
+pair2 = random.sample(pair, 150)
+K = 4
 
 def cluster_points(X, mu):
-    
-    clusters  = {}
+    clusters = {}
     for x in X:
-        bestmukey = min([(i[0], np.linalg.norm(x-mu[i[0]])) \
-                    for i in enumerate(mu)], key=lambda t:t[1])[0]
-        #print [(i[0], np.linalg.norm(x-mu[i[0]])) for i in enumerate(mu)]
-        #print bestmukey
+        bestmukey = min([(i[0], np.linalg.norm(x - mu[i[0]])) \
+                         for i in enumerate(mu)], key=lambda t: t[1])[0]
+        # print [(i[0], np.linalg.norm(x-mu[i[0]])) for i in enumerate(mu)]
+        # print bestmukey
         try:
             clusters[bestmukey].append(x)
         except KeyError:
             clusters[bestmukey] = [x]
     return clusters
 
+
 def reevaluate_centers(mu, clusters):
     newmu = []
     keys = sorted(clusters.keys())
     for k in keys:
-        newmu.append(np.mean(clusters[k], axis = 0))
+        newmu.append(np.mean(clusters[k], axis=0))
     return newmu
+
 
 def has_converged(mu, oldmu):
     return (set([tuple(a) for a in mu]) == set([tuple(a) for a in oldmu]))
+
 
 def find_centers(X, K):
     # Initialize to K random centers
@@ -75,13 +71,11 @@ def find_centers(X, K):
         clusters = cluster_points(X, mu)
         # Reevaluate centers
         mu = reevaluate_centers(oldmu, clusters)
-    return(mu, clusters)
+    return (mu, clusters)
 
 
 def init_board(N):
-
-    X = np.array([(random.uniform(-1, 1), random.uniform(-1, 1)) for i in range(N)])
-
+    X = np.array([(pair[i][0], pair[i][1]) for i in range(N)])
     return X
 
 
